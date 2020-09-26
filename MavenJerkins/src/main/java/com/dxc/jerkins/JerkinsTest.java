3fc897bd4156f97ff35e9217742a07679344d407
msaigangadha <msaigangadha@host.docker.internal>
2020-09-26 19:02:23 +0530

    First Commit

---
+++ MavenJerkins/src/main/java/com/dxc/jerkins/JerkinsTest.java
@@ -0,0 +1,12 @@
+package com.dxc.jerkins;
+
+public class JerkinsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Guys... Welcome to DXC Technology");
+
+	}
+
+}
